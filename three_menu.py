d97b63f3e8390589f25c9f2b8530b8abdb8a527f
auther_user <alanwmail@163.com>
2015-10-28 15:59:31 +0800

    Update three_menu.py

--- three_menu.py
+++ three_menu.py
@@ -2,14 +2,18 @@
 #-*- coding: utf-8 -*-
 __author__ = 'root'
 #获取输入值
-input_data = raw_input('请输入你要查找的城市：')
-def menu(input_data):
+
+def menu():
     #定义一级菜单
     One_menu = ['北京','河南','甘肃']
     #定义二级菜单
     Two_menu = {'北京':'海淀','河南':'商丘','甘肃':'肃州区'}
     #定义三级菜单
     Three_menu = {'海淀':'西二旗','商丘':'宁陵','肃州区':'丰乐'}
+    #打印一级菜单
+    for i in One_menu:
+        print i
+    input_data = raw_input('请输入你要查找的城市：')
     #判断城市是否存在于一级菜单中
     if input_data in One_menu:
         #根据一级菜单列表和二级菜单的key 三级菜单的key和二级菜单的value的对应关系进行数据抽取
@@ -19,4 +23,4 @@ def menu(input_data):
                 %s
             '''%(input_data,Two_menu[input_data],Three_menu[Two_menu[input_data]])
 if __name__ == '__main__':
-    menu(input_data)
+    menu()
